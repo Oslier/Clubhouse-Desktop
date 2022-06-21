c617489d81749076f839c29bbce939108f0750ff
Oslier <48390709+Oslier@users.noreply.github.com>
2022-06-21 19:49:21 -0400

    hover update

--- clubhouse/src/App.js
+++ clubhouse/src/App.js
@@ -32,32 +32,32 @@ function App() {
 
         <div className="tilecontainer">
           <h1>Title</h1>
-
+          
           <div className="tiles">
             <div className="column">
-              <Tile title="asd asdad asd" text="sadfsdfsdfasdfs sadfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa df">
+              <Tile title="lorem" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime nihil quis, quos exercitationem repudiandae atque! Laudantium hic aspernatur itaque odio!">
 
               </Tile>
 
-              <Tile title="asd" text="sadfsdfsdfasdfs saddfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa dfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa dfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa dfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa dfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa dfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa dfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa dfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa fsa d fsdfasdfs sadfsa fsdfasdfs sadfsafsdfasdfs sadfsafasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa df">
+              <Tile title="lorem" text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, facere? Error deleniti quis exercitationem corporis quaerat ad, consequuntur distinctio omnis. In officiis voluptatem eum, corrupti amet odio expedita provident debitis repellat cum vero, unde eos, nulla excepturi? Id error doloremque consectetur, porro hic odit sit laudantium aliquam, non facilis et.">
 
               </Tile>
               
-              <Tile title="asdasdasdsa" text="sadfsdfsdfasdfs sadfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa df dfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa dfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa dfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa dfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa ">
+              <Tile title="lorem" text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, facere? Error deleniti quis exercitationem corporis quaerat ad, consequuntur distinctio omnis. In officiis voluptatem eum, corrupti amet odio expedita provident debitis repellat cum vero, unde eos, nulla excepturi? Id error doloremque consectetur, porro hic odit sit laudantium aliquam, non facilis et.">
 
               </Tile>
             </div>
 
             <div className="column">
-              <Tile title="lorem asd" text="sadfsdfsdfasdffasdfs sadfsa dffasdfs sadfsa dfasdfasdfs sadfsa dfasdfasdfs sadfsa dfasdfasdfs sadfsa dfasdfasdfs sadfsa dfasdfasdfs sadfsa dfasdfasdfs sadfsa dfasdasdfasdfs sadfsa dfasds sadfsa fasdfs sadfsa dfasdfasdfs sadfsa dfasdfasdfs sadfsa dfasddfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa df">
+              <Tile title="lorem" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Est minima, facilis distinctio sit fuga nihil doloribus architecto numquam eaque commodi odit. Rem minus obcaecati cumque magni dignissimos, illo sint suscipit, iure ratione commodi ipsam eligendi nostrum ullam, atque ipsa possimus? Dignissimos ullam quam, eaque quisquam consequatur qui quaerat perferendis facilis incidunt magni voluptate eius unde excepturi ea quas mollitia at tempora nam deserunt ad itaque beatae ipsa illum! Illo quibusdam provident, id ex tempora architecto. Ipsa modi libero omnis at. Tenetur aut consequatur veritatis omnis amet culpa doloribus eaque atque ad perspiciatis? Consequatur nostrum omnis fugiat laudantium voluptas numquam obcaecati!">
 
               </Tile>
 
-              <Tile title="asda asdasd" text="sadfsdfsdfasdfs sadfsa dfasdfasdfs sadfsa dfasdfasdfs sadfsa dfasdfasdfs sadfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa df">
+              <Tile title="lorem" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime nihil quis, quos exercitationem repudiandae atque! Laudantium hic aspernatur itaque odio!">
 
               </Tile>
 
-              <Tile title="asdasd" text="sadfsdfsdfasdfs sadfsa dfasdfasdfsadf sadfasdfa sd fsadf sadfasdf sadf sdfsa df">
+              <Tile title="lorem" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime nihil quis, quos exercitationem repudiandae atque! Laudantium hic aspernatur itaque odio!">
 
               </Tile>
             </div>
